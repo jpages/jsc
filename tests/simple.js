be1c205835be7cdaac7e95c48c7129937421dba8
Manuel Selva <manuel.selva@inria.fr>
2017-06-14 09:07:51 +0200

    added simple.js file

---
+++ tests/simple.js
@@ -0,0 +1,20 @@
+function test(p)
+{
+    for (var i = 0; i < N; i++) {
+	var val = p[i];
+	val = val + 777777
+	val = val * 41
+	p[i] = val
+    }
+}
+
+var N = 1000000
+var flat_pixels = [];
+for (var i = 0; i < N; i++) {
+    flat_pixels[i] = i
+}
+
+test(flat_pixels);
+print(flat_pixels[777])
+test(flat_pixels);
+print(flat_pixels[777])
